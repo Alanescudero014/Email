562b9d7e7fd0534170066137cf138f5144e27df2
Alanescudero014 <252118003ajescuderoa@gmail.com>
2023-02-23 14:51:50 -0600

    javascript

---
+++ java.js
@@ -0,0 +1,25 @@
+if(localStorage.a1 != null){
+    a1 = checked=true;
+}
+
+
+
+
+function ar1(){
+    localStorage.a1 = "a1";
+}
+function ar2(){
+    localStorage.b1 = "b1";
+}
+function ar3(){
+    localStorage.c1 = "c1";
+}
+
+
+
+document.getElementById("ar1");
+document.getElementById("ar2");
+document.getElementById("ar3");
+document.getElementById("ar4");
+document.getElementById("ar5");
+document.getElementById("ar6");
